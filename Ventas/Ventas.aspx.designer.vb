6c0904fb10fb4bb0510a124ba602a185d3d41c3a
Seba <sebaperez32@gmail.com>
2022-04-13 13:56:33 -0300

    Comienzo modulo Ventas

---
+++ Ventas/Ventas.aspx.designer.vb
@@ -0,0 +1,60 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class Ventas
+
+    '''<summary>
+    '''Control formVentas.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents formVentas As Global.System.Web.UI.HtmlControls.HtmlForm
+
+    '''<summary>
+    '''Control txtProducto.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtProducto As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control lblProducto.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblProducto As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control txtCantidad.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtCantidad As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control lblPrecioTotal.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblPrecioTotal As Global.System.Web.UI.WebControls.Label
+End Class
